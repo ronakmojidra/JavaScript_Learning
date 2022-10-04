2ca6c1ef70dfc17e0ccbaeca39dbebdd95ed4ee7
ronakmojidra <mojidraronak@gmail.com>
2022-10-04 12:53:26 +0530

    Java learning
    Java learning

---
+++ demo.js
@@ -0,0 +1,7 @@
+//This is my first program
+// Now I know the two ways to run program
+console.log("Hello");
+console.log("My name is Ronak");
+console.info(" This is info"); // info means information
+console.warn(" I am not that bad guy"); //warn means warning
+console.error("Oops failed to execute");
